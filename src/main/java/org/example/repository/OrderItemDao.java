4f5d7bff1c48f91c28043c855c46e33e936fa394
khomk <khomkovdmitrii@gmail.com>
2026-07-16 16:01:07 +0200

    order complete

--- src/main/java/org/example/repository/OrderItemDao.java
+++ src/main/java/org/example/repository/OrderItemDao.java
@@ -1,7 +1,9 @@
 package org.example.repository;
 
+import org.example.domain.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class OrderItemDao {
+public interface OrderItemDao extends JpaRepository<OrderItem, Long> {
 }
